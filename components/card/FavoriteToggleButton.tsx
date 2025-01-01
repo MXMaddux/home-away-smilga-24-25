8d2bda69d23dd30896732920ece07c76565a379b
MXMaddux <maxmaddux@gmail.com>
2025-01-01 09:22:08 -0800

    Get to Build Error

--- components/card/FavoriteToggleButton.tsx
+++ components/card/FavoriteToggleButton.tsx
@@ -1,10 +1,10 @@
+import { auth } from "@clerk/nextjs/server";
 import { CardSignInButton } from "../form/Buttons";
 import { fetchFavoriteId } from "@/utils/actions";
 import FavoriteToggleForm from "./FavoriteToggleForm";
-import { useAuth } from "@clerk/nextjs";
 
 async function FavoriteToggleButton({ propertyId }: { propertyId: string }) {
-  const { userId } = useAuth();
+  const { userId } = auth();
   if (!userId) return <CardSignInButton />;
   const favoriteId = await fetchFavoriteId({ propertyId });
 
